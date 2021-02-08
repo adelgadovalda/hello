4a0b04bac62f33e3e829fe67d876ce691a9389c7
Alberto <adelgadovalda@gmail.com>
2021-02-08 16:46:02 -0300

    OK

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+//Paquetes y módulos
+//Paquete: carpeta que contiene una colección de archivo que nos entregan una funcionalidad
+
+func main() {
+	fmt.Println("Iniciando...")
+
+}
